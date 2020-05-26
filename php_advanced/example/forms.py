2beb9646b12909dfa1e054236a146ab0f327e9ee
Albert Nowak <albertnowak12@gmail.com>
2020-05-26 17:14:17 +0200

    [12] Validators

--- php_advanced/example/forms.py
+++ php_advanced/example/forms.py
@@ -4,6 +4,14 @@ from example.models import GiftList
 
 
 class GiftListForm(forms.ModelForm):
+    def clean_name(self):
+        name = self.cleaned_data['name']
+        if 'curse' in name.lower():
+            raise forms.ValidationError('You can not use forbidden words')
+
+        # always return the cleaned data, whether you have changed it or not
+        return name
+
     class Meta:
         model = GiftList
         exclude = ('modified', )
