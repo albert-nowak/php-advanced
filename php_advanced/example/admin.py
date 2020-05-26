dbc6491b6ad4330281b9512dfd7a265ffeb20378
Albert Nowak <albertnowak12@gmail.com>
2020-05-26 13:22:41 +0200

    [6] Django admin

--- php_advanced/example/admin.py
+++ php_advanced/example/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from example.models import GiftList, Gift
+
+
+class GiftInLine(admin.TabularInline):
+    model = Gift
+    extra = 1
+
+
+class GiftListAdmin(admin.ModelAdmin):
+    inlines = (GiftInLine, )
+
+
+admin.site.register(GiftList, GiftListAdmin)
+admin.site.register(Gift)
