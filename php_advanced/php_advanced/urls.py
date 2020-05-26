d913f48e2d2b6f228cec354cb379ca5e3ad97bc1
Albert Nowak <albertnowak12@gmail.com>
2020-05-26 12:56:31 +0200

    [3] First `Hello world` view

--- php_advanced/php_advanced/urls.py
+++ php_advanced/php_advanced/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from example import views as example_views
+
 urlpatterns = [
+    path('hello/', example_views.hello_world),
     path('admin/', admin.site.urls),
 ]
