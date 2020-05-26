0aa55860c9926e03ae72a247f8d32846bf4125be
Albert Nowak <albertnowak12@gmail.com>
2020-05-26 13:18:45 +0200

    [5] Gift and GiftList models with migrations

--- php_advanced/example/models.py
+++ php_advanced/example/models.py
@@ -1,3 +1,20 @@
+from datetime import datetime
+
 from django.db import models
 
-# Create your models here.
+
+class GiftList(models.Model):
+    name = models.CharField(max_length=64)
+    created_on = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(default=datetime.now)
+
+    def __str__(self):
+        return self.name
+
+
+class Gift(models.Model):
+    name = models.CharField(max_length=128)
+    gift_list = models.ForeignKey(GiftList, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
