887a75cec9eb81cda3689df42f2b88402bfd5832
Albert Nowak <albertnowak12@gmail.com>
2020-05-26 13:58:00 +0200

    [8] Generic ListView instead of function view

--- php_advanced/example/views.py
+++ php_advanced/example/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic import ListView
 
 from example.models import GiftList
 
@@ -12,10 +13,7 @@ def hello_name(request, name):
     return HttpResponse(f'Hello, {name}!')
 
 
-def simple_list_view(request):
-    gfl_entries = GiftList.objects.all()
-    return render(
-        request,
-        'example/list.html',
-        {'gfl_entries': gfl_entries}
-    )
+class GiftListListView(ListView):
+    model = GiftList
+    template_name = 'example/list.html'
+    context_object_name = 'gfl_entries'
